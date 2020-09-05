b66708ba11597d1ab3435163344a9b3a67034b99
Xenofon Alexakos <xalexakos@gmail.com>
2020-09-05 11:46:17 +0300

    - created user login view & process.

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('registration/', views.user_registration_view, name='user_registration_view'),
     path('logout/', views.user_logout_view, name='user_logout_view'),
+    path('login/', views.user_login_view, name='user_login_view'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
-from django.contrib.auth import login, logout
+from django.contrib import messages
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 
 from users.decorators import anonymous_user
@@ -28,3 +30,23 @@ def user_logout_view(request):
     """ Terminates user session and redirects to index page. """
     logout(request)
     return redirect('movies_list_view')
+
+
+@anonymous_user
+def user_login_view(request):
+    form = AuthenticationForm()
+    context = {'form': form}
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('movies_list_view')
+        else:
+            messages.add_message(request, messages.ERROR, 'Invalid username or password')
+
+    return render(request, 'users/login.html', context)
